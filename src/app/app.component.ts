11e50eefdbce297c0cc11da5fb882d3acfb60a42
glots-a <glots@ukr.net>
2023-07-13 20:51:13 +0300

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,22 +9,45 @@ export class AppComponent {
   password: string = '';
   passwordStrength: string = '';
   passwordStrengthPercentage: number = 0;
+  inputState: boolean = false;
 
-  checkPasswordStrength() {
+  toggle(): void {
+    this.inputState = !this.inputState;
+  }
+
+  checkPasswordStrength(): void {
     const lowercaseRegex = /^(?=.*[a-z]).+$/;
     const uppercaseRegex = /^(?=.*[A-Z]).+$/;
     const numberRegex = /^(?=.*[0-9]).+$/;
     const specialCharRegex = /^(?=.*[!@#$%^&*]).+$/;
 
-    if (this.password.length < 8) {
-      this.passwordStrength = 'Weak';
+    if (this.password.length === 0) {
+      this.passwordStrength = '';
       this.passwordStrengthPercentage = 0;
-    } else if (lowercaseRegex.test(this.password) && uppercaseRegex.test(this.password) && numberRegex.test(this.password) && specialCharRegex.test(this.password)) {
+    } else if (this.password.length < 8) {
+      this.passwordStrength = 'Weak';
+      this.passwordStrengthPercentage = 25;
+    } else if (
+      lowercaseRegex.test(this.password) &&
+      uppercaseRegex.test(this.password) &&
+      numberRegex.test(this.password) &&
+      specialCharRegex.test(this.password)
+    ) {
       this.passwordStrength = 'Strong';
       this.passwordStrengthPercentage = 100;
-    } else {
+    } else if (
+      (lowercaseRegex.test(this.password) && uppercaseRegex.test(this.password)) ||
+      (lowercaseRegex.test(this.password) && numberRegex.test(this.password)) ||
+      (lowercaseRegex.test(this.password) && specialCharRegex.test(this.password)) ||
+      (uppercaseRegex.test(this.password) && numberRegex.test(this.password)) ||
+      (uppercaseRegex.test(this.password) && specialCharRegex.test(this.password)) ||
+      (numberRegex.test(this.password) && specialCharRegex.test(this.password))
+    ) {
       this.passwordStrength = 'Medium';
-      this.passwordStrengthPercentage = 50;
+      this.passwordStrengthPercentage = 75;
+    } else {
+      this.passwordStrength = 'Weak';
+      this.passwordStrengthPercentage = 25;
     }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    BrowserAnimationsModule
   ],
   bootstrap: [AppComponent]
 })
